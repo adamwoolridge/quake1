041050f6559e3fdb2d7e174913b0237c06f8279e
Adam Woolridge <a.woolridge@gmail.com>
2016-08-14 16:54:49 +0100

    Missed file

---
+++ Assets/Scripts/BSP/Types/BSPEdge.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BSPEdge 
+{
+	public ushort startIndex;
+	public ushort endIndex;
+
+	public BSPEdge( ushort start, ushort end )
+	{
+		startIndex = start;
+		endIndex = end;
+	}
+}
