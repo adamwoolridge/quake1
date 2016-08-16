e8150b54e4b2d7a132e3ae5373fac8dfe6fc2176
Adam Woolridge <a.woolridge@gmail.com>
2016-08-16 07:32:15 +0100

    MDL uv WIP

--- Assets/Scripts/MDL/MDL.cs
+++ Assets/Scripts/MDL/MDL.cs
@@ -61,30 +61,53 @@ public class MDL
             frames.Add( new MDLFrame( mdlFile, header ) );
     }
 
+    // Just getting it working, will clean up when it works
     public void DrawFrame( int frame )
     {
         Vector3 [] allVerts = new Vector3[ header.vertCount ];
-        //int[] triangles = new int[ header.triCount];
-
+        int[] tris = new int[ header.triCount * 3];
+        Vector2[] uvs = new Vector2[ header.vertCount ];
 
         for ( int i = 0; i < header.triCount; i++ )
         {
-            Vector3[] vertices = new Vector3[ 3];
+            Vector3[] vertices = new Vector3[ 3 ];
 
             for ( int v = 0; v < 3; v ++ )
             {
-                MDLVert thisVert = frames[frame].verts[triangles[i].vertexIndexes[v]];
-                vertices[v] = new Vector3();
-                vertices[v].x = (header.scale.x * thisVert.v[0]) + header.translate[0];
-                vertices[v].y = (header.scale.y * thisVert.v[2]) + header.translate[2];
-                vertices[v].z = (header.scale.z * thisVert.v[1]) + header.translate[1];
+                MDLVert thisVert = frames[ frame ].verts[ triangles[ i ].vertexIndexes[ v ] ];
+                int index = triangles[ i ].vertexIndexes[ v ];
 
-                
+                allVerts[ index ] = new Vector3();
+                allVerts[ index ].x = ( header.scale.x * thisVert.v[ 0 ] ) + header.translate[ 0 ];
+                allVerts[ index ].y = ( header.scale.y * thisVert.v[ 2 ] ) + header.translate[ 2 ];
+                allVerts[ index ].z = ( header.scale.z * thisVert.v[ 1 ] ) + header.translate[ 1 ];
+
+                float s = texCoords[triangles[i].vertexIndexes[v]].s;
+                float t = texCoords[triangles[i].vertexIndexes[v]].t;
+
+                if (triangles[i].facesFront != 1 && texCoords[triangles[i].vertexIndexes[v]].onSeam != 0)
+                {
+                    s += (float)header.skinWidth * 0.5f;
+                }
+
+                s = (s + 0.5f) / header.skinWidth;
+                t = (t + 0.5f) / header.skinHeight;
+
+                uvs[index] = new Vector2(s, t);
             }
 
-            Debug.DrawLine(vertices[0], vertices[1], Color.white, 10000f);
-            Debug.DrawLine(vertices[1], vertices[2], Color.white, 10000f);
-            Debug.DrawLine(vertices[2], vertices[0], Color.white, 10000f);
+            tris[ i * 3 ] = triangles[ i ].vertexIndexes[ 0 ];
+            tris[ i * 3 + 1 ] = triangles[ i ].vertexIndexes[ 1 ];
+            tris[ i * 3 + 2 ] = triangles[ i ].vertexIndexes[ 2 ];            
         }
+
+        GameObject faceObj = new GameObject();
+        Mesh mesh = new Mesh();
+        mesh.vertices = allVerts;        
+        mesh.triangles = tris;
+        mesh.uv = uvs;
+        mesh.RecalculateNormals();
+        faceObj.AddComponent<MeshFilter>().mesh = mesh;
+        MeshRenderer rend = faceObj.AddComponent<MeshRenderer>();
     }
 }
